3d4b155f0aff907a5e2b665d54e5358b1fd854ae
Vector400 <ronaldvector210@gmail.com>
2025-09-20 00:14:21 -0500

    Subida del ejercicio 1 en C#

---
+++ PR_S5-C#/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_S5_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio1();
+            Console.ReadKey();
+        }
+
+        static void ejercicio1()
+        {
+            double costoProducto, rebaja = 0, pagoFinal, subtotal, bonoExtra = 0;
+            int tipoUsuario;
+
+            // Mensaje inicial
+            Console.WriteLine("===== SISTEMA DE DESCUENTOS =====");
+            Console.Write("\nIngrese el costo unitario del artículo: ");
+            costoProducto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSeleccione el tipo de descuento:\n[1] Estudiante\n[2] Empleado\n[3] Cliente frecuente");
+            Console.Write("\nIngrese su opción: ");
+            tipoUsuario = int.Parse(Console.ReadLine());
+
+            // Validaciones anidadas
+            if (costoProducto >= 0)
+            {
+                if (tipoUsuario <= 3 && tipoUsuario > 0)
+                {
+                    if (tipoUsuario == 1)
+                    {
+                        rebaja = costoProducto * 0.10;
+                    }
+                    else if (tipoUsuario == 2)
+                    {
+                        rebaja = costoProducto * 0.15;
+                    }
+                    else
+                    {
+                        rebaja = costoProducto * 0.20;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nERROR: Tipo de descuento inválido.");
+                }
+
+                subtotal = costoProducto - rebaja;
+
+                if (costoProducto > 500)
+                    bonoExtra = subtotal * 0.05;
+
+                pagoFinal = subtotal - bonoExtra;
+
+                Console.WriteLine($"\nTotal a pagar luego del descuento: {pagoFinal:F2}");
+            }
+            else
+            {
+                Console.WriteLine("\nERROR: El costo ingresado no es válido.");
+            }
+        }
+    }
+}
---
+++ PR_S5-C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("PR_S5-C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PR_S5-C#")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b5d39657-5036-4fa5-b10f-b00ddd00aa6b")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
